0f1f981f06b05cebf06c2f802fd2ba5354eaea0f
RSA <ravils465@gmail.com>
2024-10-16 18:10:12 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app/app.module'
 async function StartApp() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'https://threegis-frontnend.onrender.com/',
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
